85906a4417ed617a01eeca7e93c06e4c66b0e78e
Ewa Tryniszewska <eva.tryniszewska@gmail.com>
2020-03-10 17:09:09 +0100

    lab2

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -9,16 +9,35 @@ public class Animal {
     File pic;
 
     static final Double DEFAULT_DOG_WEIGHT = 8.0;
+    static final Double DEFAULT_LION_WEIGHT = 190.0;
+    static final Double DEFAULT_MOUSE_WEIGHT = 0.05;
 
     public Animal(String species) {
         this.species = species;
-        if(species == "dog") {
+        if (species == "dog") {
             this.weight = DEFAULT_DOG_WEIGHT;
+        } else if (species == "lion") {
+            this.weight = DEFAULT_LION_WEIGHT;
+        } else if (species == "mouse") {
+            this.weight = DEFAULT_MOUSE_WEIGHT;
         }
     }
 
     void feed() {
-        weight++;
-        System.out.println("thanks, my weight is now " + weight);
+        if (this.weight > 0) {
+            this.weight++;
+            System.out.println("Thanks, my weight is now " + this.weight);
+        } else {
+            System.out.println("You can't feed me. I'm dead.");
+        }
+    }
+
+    void takeAWalk() {
+        if (this.weight > 0) {
+            this.weight--;
+            System.out.println("Thanks for a walk");
+        } else {
+            System.out.println("I'm dead - you can't go for a walk with me");
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
         Animal dog = new Animal("dog");
-	    dog.name = "Akita";
+        dog.name = "Akita";
 
-	    Animal lion = new Animal("lion");
-	    lion.name = "Leo";
+        Animal lion = new Animal("lion");
+        lion.name = "Leo";
 
-	    Human me = new Human();
-	    me.firstName = "Ewa";
-	    me.lastName = "Tryniszewska";
-	    me.pet = dog;
+        Human me = new Human();
+        me.firstName = "Ewa";
+        me.lastName = "Tryniszewska";
+        me.pet = new Animal("lion");
 
-	    me.pet.feed();
+        me.pet.feed();
 
-		System.out.println(me.pet.name);
+        me.pet = dog;
+        me.pet.feed();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+        me.pet.takeAWalk();
+
+        System.out.println(me.pet.name);
     }
 }
