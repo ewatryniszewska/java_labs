bb9671ce659f6188966a63934d8cdc89af3f85d6
Ewa Tryniszewska <eva.tryniszewska@gmail.com>
2020-04-07 17:18:51 +0200

    Task 6 - Share a car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ public class Main {
         lion.name = "Leo";
 
         Human me = new Human();
+
         me.firstName = "Ewa";
         me.lastName = "Tryniszewska";
         me.pet = new Animal("lion");
@@ -18,12 +19,23 @@ public class Main {
         me.pet.feed();
         me.pet.takeAWalk();
 
-        me.setCar(new Car("Opel", 1000.0, "Astra"));
-        me.getCar().registrationNumber = "GD9785";
-        me.getCar().color = "Red";
+        Car myCar = new Car("Opel", 1000.0, "Astra");
+        myCar.registrationNumber = "GD9785";
+        myCar.color = "Red";
+
+        me.setCar(myCar);
+
+        Human myHusband = new Human();
+        myHusband.firstName = "Imie meza";
+        myHusband.lastName = "Nazwisko meza";
+        myHusband.setCar(myCar);
 
         System.out.println(me.getCar().brand + " " + me.getCar().model + " " + me.getCar().engine + " "
                 + me.getCar().color + " " + me.getCar().registrationNumber);
+        System.out.println(me.getCar());
+        System.out.println(myHusband.getCar());
+
+        System.out.println(new Car("Opel", 1000.0, "Astra"));
 
         System.out.println("I earn " + me.getSalary());
         me.setSalary(1500.0);
